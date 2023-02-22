229f5344750521b5c188b13a9fcde9b55759ac5a
Aya-Abdellatif <ayaabdllatif112@gmail.com>
2023-02-21 17:44:56 -0800

    task2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* print_alphabet_x10 - prints alphabets in lower case 10 times
+*
+*/
+
+void print_alphabet_x10(void)
+{
+	int i;
+	char ch;
+
+	ch = 'a';
+
+	for (i = 0; i < 10; i++)
+	{
+		while (ch <= 'z')
+		{
+			_putchar(ch);
+		}
+		_putchar('\n');
+	}
+}
