5cf1e2a6ab192042f329b7468a7e6b719022626b
Aya-Abdellatif <106776143+Aya-Abdellatif@users.noreply.github.com>
2023-03-14 01:48:31 +0200

    2-strlen.c

--- 0x09-static_libraries/2-strlen.c
+++ 0x09-static_libraries/2-strlen.c
@@ -1,1 +1,21 @@
-int _strlen(char *s);
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: the length of a string
+ */
+
+int _strlen(char *s)
+{
+
+	int i;
+
+	i = 0;
+	while (*s != '\0')
+	{
+		s++;
+		i++;
+	}
+	return (i);
+}
