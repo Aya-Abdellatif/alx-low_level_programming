cf03f5a5638a3979e9f7b077c3f4f51477ce35d3
Aya-Abdellatif <106776143+Aya-Abdellatif@users.noreply.github.com>
2023-04-05 21:03:23 +0200

    update 0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,12 +8,13 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t i = 0;
+	size_t siz = 0;
 
 	while (h)
 	{
 		printf("%d\n", h->n);
-		h = h->next, i++;
+		h = h->next;
+		siz++;
 	}
-	return (i);
+	return (siz);
 }
