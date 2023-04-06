55c2151b3d53ddafb6e8f53f2378fa0b4b9d93bb
Aya-Abdellatif <106776143+Aya-Abdellatif@users.noreply.github.com>
2023-04-06 06:09:56 +0200

    Update 5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,17 +7,18 @@
  *
  *Return: nothing
  */
+
 void free_listint2(listint_t **head)
 {
-	listint_t *tmp;
+	listint_t *temp;
 
 	if (!head)
 		return;
 	while (*head)
 	{
-		tmp = *head;
+		temp = *head;
 		*head = (*head)->next;
-		free(tmp);
+		free(temp);
 	}
 	*head = NULL;
 }
